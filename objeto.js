e77923116681f61fe3c95dad1a2ee7dfa1020369
Maikon Monteiro <maikonsm7@gmail.com>
2024-04-18 19:59:03 -0400

    create objeto.js

---
+++ objeto.js
@@ -0,0 +1,11 @@
+// Objeto é um conjunto de par chave e valor
+const prod1 = {} // forma literal para criar um obj
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4800
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90
+}
+console.log(prod2)
