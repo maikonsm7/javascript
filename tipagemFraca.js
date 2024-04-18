f644f09bcae912ecfc7426ff2fa3373843a310e3
Maikon Monteiro <maikonsm7@gmail.com>
2024-04-18 17:37:50 -0400

    update tipagemFraca

--- tipagemFraca.js
+++ tipagemFraca.js
@@ -1,1 +1,8 @@
 let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// é possível mudar o tipo da variável a qualquer momento
+qualquer = 4
+console.log(qualquer)
+console.log(typeof qualquer)
