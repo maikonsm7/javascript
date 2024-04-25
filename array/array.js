9355b510f4a088682331e7441d8e072e258d0b0e
Maikon Monteiro <maikonsm7@gmail.com>
2024-04-25 15:49:19 -0400

    update array

--- array/array.js
+++ array/array.js
@@ -12,5 +12,11 @@ console.log(valores)
 valores.pop() // remove o último elemento
 console.log(valores)
 
+valores.unshift(6.5) // adiciona no início
+console.log(valores)
+
+valores.shift() // remove o primeiro elemento
+console.log(valores)
+
 delete valores[1] // deleta o elemento, mas o índice fica vazio
 console.log(valores)
