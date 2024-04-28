ed55fde163987809148584111ee961e02f907b25
Maikon Monteiro <maikonsm7@gmail.com>
2024-04-28 19:49:33 -0400

    create objetoRevisao

---
+++ objeto/objetoRevisao.js
@@ -0,0 +1,9 @@
+// Objeto é uma coleção de pares chave/valor
+const produto = new Object
+produto.nome = 'Cadeira'
+produto.preco = 220
+
+console.log(produto)
+
+delete produto.preco
+console.log(produto)
---
+++ objeto/oo.js
@@ -0,0 +1,19 @@
+/*
+CÓDIGO NÃO EXECUTÁVEL
+
+Não Procedural
+go to -> programação não estrutural
+
+Procedural
+procedimento(valor1, valor2) -> trecho de código que pode ser usado em qualquer parte do sistema
+você tem funções que manipulam dados
+
+OO -> a idéia é estar mais próximo do mundo real
+você tem dados que tem funções dentro
+o foco passou a ser o objeto
+Princípio importantes:
+1. Abstração - Abstrair objetos do mundo real para seu sistema
+2. Encapsulamento - Detalhes de funcionamento escondido. Mostrar apenas o que é necessário. Você lida apenas com a interface de comunicação.
+3. Herança - Herda os atributos e métodos do pai (Prototype)
+4. Polimorfismo - Usar conceitos mais gerais para mais específicos (Carro, Ferrari)
+*/
