6f4fd5cf87ac7db0a5576620b4131df75a4bb16a
Maikon Monteiro <maikonsm7@gmail.com>
2024-04-30 14:40:53 -0400

    update array

--- array/array.js
+++ array/array.js
@@ -20,3 +20,15 @@ console.log(valores)
 
 delete valores[1] // deleta o elemento, mas o índice fica vazio
 console.log(valores)
+
+const nums = [4,1,3,5,2]
+nums.sort() // organizar array em ordem crescente
+console.log(nums)
+
+nums.splice(1, 2) // remover, adicionar, remover e adicionar elementos de um array
+// nums.splice(2, 0, 'novo item', 'outro novo item')
+console.log(nums)
+
+const subArray = nums.slice(1) // equivalente ao substring
+// const subArray = nums.slice(1, 3) // com intervalo
+console.log(subArray)
