356c71cd5e7b6ddd97c0aae52860bb197fbc1d4e
Maikon Monteiro <maikonsm7@gmail.com>
2024-04-30 16:51:31 -0400

    create concat

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+// concatenar arrays
+const filhas = ['Ana', 'Lia']
+const filhos = ['Marcos', 'Lucas']
+const todos = filhas.concat(filhos)
+console.log(todos)
+console.log(todos.concat(['Victor', 'José']))
