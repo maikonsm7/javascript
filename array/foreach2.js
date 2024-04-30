d4b5ba10001a22d24d8e299fe6f8a6fa395e8395
Maikon Monteiro <maikonsm7@gmail.com>
2024-04-30 15:02:03 -0400

    update foreach2

--- array/foreach2.js
+++ array/foreach2.js
@@ -1,9 +1,9 @@
 // recreando o foreach
-const foreach2 = (arr, func) => {
-    for(let i = 0; i < arr.length; i++){
-        func(arr[i], i, arr)
+Array.prototype.forEach2 = function (callback){
+    for(let i = 0; i < this.length; i++){
+        callback(this[i], i, this)
     }
 }
 
 const aprovados = ['Ana', 'Marcos', 'Lucas']
-foreach2(aprovados, function (nome, indice){console.log(`${indice}. ${nome}`)})
+aprovados.forEach2(function (nome, indice){console.log(`${indice}. ${nome}`)})
