3027f209a7e531d4e31d2ce7a8a7d7dda79c62b9
Maikon Monteiro <maikonsm7@gmail.com>
2024-05-05 18:46:41 -0400

    create forOf

---
+++ forOf.js
@@ -0,0 +1,9 @@
+// laço para iterar sobre os valores de uma estrutura
+for(let letra of 'Maikon'){
+    console.log(letra)
+}
+
+const valores = [1, 2, 3, 4]
+for(let i of valores){
+    console.log(i)
+}
